e5e5285cb6ad8ff341d9feb344bc00e51bd7fe16
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-13 16:56:30 +0500

    navbar css completed

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
-          <Route path="/cart" />
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </div>
--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import "../components/navbar.css";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="nav-bar">
+      <div className="link">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
