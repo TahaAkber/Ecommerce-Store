e7b0a33422a5296bac075560165be77edbf0ce95
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-15 23:15:05 +0500

    Update context.jsx

--- my-app/src/pages/context/shop-context.tsx
+++ my-app/src/pages/context/shop-context.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { createContext } from "react";
 
+export const ShopContext = createContext(null);
 const Shop_contextprovider = () => {
   return <div>shop-context</div>;
 };
