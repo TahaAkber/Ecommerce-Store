77d99cf38e442a6341c0a777965fa1ed89ac35e2
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-16 23:27:33 +0500

    addto cart functionality
    yahan pe kara yeh hai k agar kisi items me bar bar click krenge tou uska ccount increase hoga tou humne aik state leke setcart me prev item ka object lia thn phir uske itemid ko +1 krdia with respect to previous usse pehle humlog ne all products k id ko 0 krdia tha loop laga k

--- my-app/src/pages/context/shop-context.tsx
+++ my-app/src/pages/context/shop-context.tsx
@@ -5,12 +5,16 @@ import { Products } from "../../configs/Products";
 export const ShopContext = createContext(null);
 const defaultcart = () => {
   let cart = {};
-  for (let i = 1; i < Products.length, i++; ) {
+  for (let i = 1; i < Products.length + 1, i++; ) {
     cart[i] = 0;
   }
   return cart;
 };
 export const Shop_contextprovider = (props: any) => {
   const [cart, setcart] = useState(defaultcart());
+  const addtocart = (itemId) => {
+    setcart((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
   return <ShopContext.Provider value={}>{props.children}</ShopContext.Provider>;
 };
