8d2a55780c324872fe418897979a2c8e5065cce2
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-24 23:16:51 +0500

    adding auth0 into ecommerce website

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+if (!domain) {
+  throw new Error("Auth0 domain is not defined");
+}
+if (!clientId) {
+  throw new Error("Auth0 id is not defined");
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
